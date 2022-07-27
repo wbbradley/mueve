ef42049d5c4330b7dbc69d7ab7a554c74ce579bd
Will Bradley <williambbradley@gmail.com>
2022-07-26 22:45:03 -0600

    use paco

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use paco::{digits, ParseState, Progress};
 
 fn main() {
-    let ps = ParseState::new("Hello, world!");
+    let ps = ParseState::new(&"14234".to_string());
     match digits(ps) {
         Progress::Parsed(_, num) => {
             println!("{}", num);
