8c8f21852b765894241233ff2f0cfbc862eb7134
Will Bradley <williambbradley@gmail.com>
2022-08-01 23:02:46 -0600

    iterating

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,50 @@
-use paco::{digits, ParseState, Progress};
+use paco::{
+    all_of_input, concat, exact_string, lift, many, sequence, skip_whitespace, string_while,
+    strip_whitespace, ParseState, Parser, Progress,
+};
+
+fn is_ident_starter(ch: char) -> bool {
+    ch.is_ascii_alphabetic() || ch == '_'
+}
+
+fn is_ident_char(ch: char) -> bool {
+    ch.is_alphanumeric() || ch == '_'
+}
+
+fn keyword<'a, T>(kw: T) -> impl Parser<char, String>
+where
+    T: Into<String>,
+{
+    skip_whitespace(exact_string(kw.into()))
+}
+
+#[derive(Debug)]
+pub struct FnDecl {
+    name: String,
+}
+
+fn ast_func(terms: Vec<String>) -> FnDecl {
+    FnDecl {
+        name: terms[1].clone(),
+    }
+}
 
 fn main() {
-    let ps = ParseState::new(&"14234".to_string());
-    match digits(ps) {
-        Progress::Parsed(_, num) => {
-            println!("{}", num);
+    let ps = ParseState::new("fn __hello   \t fn   exit");
+
+    let identifier = skip_whitespace(lift(
+        concat,
+        sequence!(string_while(is_ident_starter), string_while(is_ident_char)),
+    ));
+
+    // TODO: implement variadic lift.
+    let func_def = lift(ast_func, sequence!(keyword("fn"), identifier));
+
+    let ast_parser = all_of_input(strip_whitespace(many(func_def)));
+
+    match ast_parser(ps) {
+        Progress::Parsed(_, value) => {
+            println!("{:?}", value);
         }
         Progress::Failed => {
             println!("Did not parse.")
