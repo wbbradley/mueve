42ac2c8d876eb09d3f4a9e6d4e57dd5686b56dcf
Will Bradley <williambbradley@gmail.com>
2022-08-07 20:34:36 -0600

    get the lexer going

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,10 +1,11 @@
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub enum Lexeme {
-    Integer(i64),
+#[derive(Debug, Clone, PartialEq)]
+#[allow(dead_code)]
+pub enum Lexeme<'a> {
+    Unsigned(u64),
     Float(f64),
-    Identifier(String),
-    QuotedString(String),
-    Operator(String),
+    Identifier(&'a str),
+    QuotedString(&'a str),
+    Operator(&'a str),
     LParen,
     RParen,
     LSquare,
@@ -12,24 +13,161 @@ pub enum Lexeme {
     LCurly,
     RCurly,
     Colon,
-    EndOfLine
+    Semicolon,
+    Dot,
+    Ampersand,
 }
 
-pub struct Location {
-    filename: &str,
+#[derive(Debug, Clone, PartialEq)]
+#[allow(dead_code)]
+pub struct Location<'a> {
+    filename: &'a str,
     line: i32,
     col: i32,
 }
 
-pub struct Token {
-    location: Location,
-    lexeme: Lexeme,
+#[derive(Debug, Clone, PartialEq)]
+#[allow(dead_code)]
+pub struct Token<'a> {
+    location: Location<'a>,
+    lexeme: Lexeme<'a>,
 }
 
+#[allow(dead_code)]
 pub struct Lexer<'a> {
-    text: mut &'a str,
+    contents: &'a str,
+    location: Location<'a>,
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token<'a>;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.contents.len() == 0 {
+            return None;
+        }
+
+        enum LS {
+            Start,
+            Identifier,
+            Digits,
+        }
+        let mut ls = LS::Start;
+        let mut start_location = self.location.clone();
+        let mut count = 0;
+        let mut lexeme_start = self.contents;
+        let mut lexeme_start_index = 0;
+        let mut ch_iter = self.contents.chars();
+        loop {
+            let ch: char = ch_iter.next().unwrap_or('\0');
+            self.update_loc(ch);
+
+            match ls {
+                LS::Start => {
+                    if ch == '\0' {
+                        return None;
+                    } else if ch.is_whitespace() {
+                    } else if ch.is_digit(10) {
+                        ls = LS::Digits;
+                        lexeme_start_index = count;
+                        lexeme_start = &self.contents[count..];
+                        start_location = self.location.clone();
+                    } else if ch == '_' || ch.is_alphabetic() {
+                        ls = LS::Identifier;
+                        lexeme_start_index = count;
+                        lexeme_start = &self.contents[count..];
+                        start_location = self.location.clone();
+                    } else if ch == ':' {
+                        return self._advance(ch, count, Lexeme::Colon);
+                    } else if ch == ';' {
+                        return self._advance(ch, count, Lexeme::Semicolon);
+                    } else if ch == '&' {
+                        return self._advance(ch, count, Lexeme::Ampersand);
+                    } else if ch == '.' {
+                        return self._advance(ch, count, Lexeme::Dot);
+                    } else if ch == '(' {
+                        return self._advance(ch, count, Lexeme::LParen);
+                    } else if ch == ')' {
+                        return self._advance(ch, count, Lexeme::RParen);
+                    } else if ch == '[' {
+                        return self._advance(ch, count, Lexeme::LSquare);
+                    } else if ch == ']' {
+                        return self._advance(ch, count, Lexeme::RSquare);
+                    } else if ch == '{' {
+                        return self._advance(ch, count, Lexeme::LCurly);
+                    } else if ch == '}' {
+                        return self._advance(ch, count, Lexeme::RCurly);
+                    } else {
+                        assert!(false, "could not figure out what do do with '{ch}'!");
+                    }
+
+                    count += ch.len_utf8();
+                }
+                LS::Identifier => {
+                    if ch == '_' || ch.is_alphanumeric() {
+                        count += ch.len_utf8();
+                    } else {
+                        self.contents = &self.contents[count..];
+                        return Some(Token {
+                            location: start_location,
+                            lexeme: Lexeme::Identifier(&lexeme_start[..count - lexeme_start_index]),
+                        });
+                    }
+                }
+                LS::Digits => {
+                    if ch.is_digit(10) {
+                        count += ch.len_utf8();
+                    } else {
+                        self.contents = &self.contents[count..];
+                        return Some(Token {
+                            location: start_location,
+                            lexeme: Lexeme::Unsigned(
+                                lexeme_start[..count - lexeme_start_index]
+                                    .parse::<u64>()
+                                    .unwrap(),
+                            ),
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.contents.len()))
+    }
 }
 
 impl<'a> Lexer<'a> {
-    fn next_token<'a>(self) -> Lexeme
+    fn _advance(&mut self, ch: char, mut count: usize, lexeme: Lexeme<'a>) -> Option<Token<'a>> {
+        count += ch.len_utf8();
+        self.contents = &self.contents[count..];
+        return Some(Token {
+            location: self.location.clone(),
+            lexeme: lexeme,
+        });
+    }
+
+    pub fn new<T>(filename: T, input: T) -> Self
+    where
+        T: Into<&'a str>,
+    {
+        Lexer {
+            contents: input.into(),
+            location: Location {
+                filename: filename.into(),
+                line: 1,
+                col: 0,
+            },
+        }
+    }
+
+    #[inline]
+    fn update_loc(&mut self, ch: char) {
+        if ch == '\n' {
+            self.location.line += 1;
+            self.location.col = 0;
+        } else {
+            self.location.col += 1;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,5 @@
-use paco::{
-    all_of_input, bracket, choice, concat, exact_string, lift, many, sequence, skip_whitespace,
-    string_while, strip_whitespace, ParseState, Parser, Progress,
-};
-
-fn is_ident_starter(ch: char) -> bool {
-    ch.is_ascii_alphabetic() || ch == '_'
-}
-
-fn is_var_ident_starter(ch: char) -> bool {
-    (ch.is_ascii_alphabetic() && ch.is_lowercase()) || ch == '_'
-}
-
-fn is_ident_char(ch: char) -> bool {
-    ch.is_alphanumeric() || ch == '_'
-}
-
-fn token<'a, T>(kw: T) -> impl Parser<char, String>
-where
-    T: Into<String>,
-{
-    skip_whitespace(exact_string(kw.into()))
-}
+mod lexer;
+use lexer::Lexer;
 
 #[derive(Debug, Clone)]
 #[allow(dead_code)]
@@ -92,141 +71,17 @@ pub struct Decl {
     body: Expr,
 }
 
-fn lift4ac<'a, A, B, C, D, V, F, PA, PB, PC, PD>(
-    f: F,
-    pa: PA,
-    pb: PB,
-    pc: PC,
-    pd: PD,
-) -> impl Parser<char, V>
-where
-    A: 'a,
-    B: 'a,
-    C: 'a,
-    D: 'a,
-    V: 'a,
-    F: 'a + Fn(A, C) -> V,
-    PA: Parser<char, A>,
-    PB: Parser<char, B>,
-    PC: Parser<char, C>,
-    PD: Parser<char, D>,
-{
-    move |ps: ParseState<char>| -> Progress<char, V> {
-        return match pa(ps) {
-            Progress::Failed => Progress::Failed,
-            Progress::Parsed(ps, a) => match pb(ps) {
-                Progress::Failed => Progress::Failed,
-                Progress::Parsed(ps, _) => match pc(ps) {
-                    Progress::Failed => Progress::Failed,
-                    Progress::Parsed(ps, c) => match pd(ps) {
-                        Progress::Failed => Progress::Failed,
-                        Progress::Parsed(ps, _) => return Progress::Parsed(ps, f(a, c)),
-                    },
-                },
-            },
-        };
-    }
-}
-
-fn ast_func(predicate: Vec<String>, callsite: String) -> FnDecl {
-    FnDecl {
-        name: predicate.join(",") + " " + &callsite,
-    }
-}
-
 fn main() {
-    let ps = ParseState::new("predicate = callsite;\t\npredicate  =callsite ;");
-
-    let ast_parser = get_ast_parser();
-
-    match ast_parser(ps) {
-        Progress::Parsed(_, value) => {
-            println!("{:?}", value);
-        }
-        Progress::Failed => {
-            println!("Did not parse.")
-        }
+    let input = "123454 14 \n pi.(} &";
+    let lexer = Lexer::new("raw-text", &input);
+    for token in lexer {
+        println!("{:?}", token);
     }
-}
-
-fn get_ast_parser() -> impl Parser<char, Vec<FnDecl>> {
-    let identifier = skip_whitespace(lift(
-        concat,
-        sequence!(
-            string_while(is_var_ident_starter),
-            string_while(is_ident_char)
-        ),
-    ));
-    let int = |s: String| -> i64 {
-        let i = s.parse::<i64>().unwrap();
-        i
-    };
-    /*
-     *
-    Irrefutable(Id),
-    Integer(i64),
-    String(String),
-    Tuple { dims: Vec<Box<Predicate>> },
-    */
-    let tuple_predicate_parser = strip_whitespace(bracket(
-        '(',
-        ')',
-        |ps: ParseState<char>| -> Progress<char, Vec<Box<Predicate>>> {
-            Progress::Parsed(ps, Vec::new())
-        },
-    ));
-    let predicate_parser = strip_whitespace(choice!(
-        lift(Predicate::Irrefutable.Id, identifier),
-        lift(Predicate::Integer, int),
-    ));
-
-    let callsite_parser = strip_whitespace(exact_string("callsite"));
-
-    let def_parser = lift5abd(
-        ast_func,
-        identifier,
-        predicate_parser,
-        token("="),
-        callsite_parser,
-        token(";"),
-    );
-
-    return all_of_input(many(strip_whitespace(def_parser)));
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    macro_rules! test_parse_ok {
-        ($input: expr, $parser: expr) => {
-            let ps: ParseState<_> = ParseState::new($input);
-            let language_parser = $parser;
-            match language_parser(ps) {
-                Progress::Parsed(_, _) => (),
-                Progress::Failed => assert!(false),
-            }
-        };
-    }
-    macro_rules! test_parse_fail {
-        ($input: expr, $parser: expr) => {
-            let ps: ParseState<_> = ParseState::new($input);
-            let language_parser = $parser;
-            match language_parser(ps) {
-                Progress::Parsed(_, _) => assert!(false),
-                Progress::Failed => (),
-            }
-        };
-    }
-
     #[test]
-    fn parse_ast() {
-        test_parse_ok!(
-            "predicate = callsite;\t\npredicate  =callsite ;",
-            get_ast_parser()
-        );
-        test_parse_fail!(
-            "predicate  callsite;\t\npredicate  =callsite ;",
-            get_ast_parser()
-        );
-    }
+    fn lex_some() {}
 }
